4494ee8a711d8475aa482328b72a5454cb49d80c
Mihir Patel <mihirkpatel8829@gmail.com>
2023-02-23 12:33:49 -0500

    Added New Files

---
+++ script.js
@@ -0,0 +1,13 @@
+const card = document.querySelector('.card');
+const message = document.querySelector('#message');
+const image = document.querySelector('#image');
+
+const messages = [
+	'May your birthday be filled with lots of love, joy, and happiness.',
+	'Wishing you a very happy birthday and a year filled with blessings.',
+	'Sending birthday wishes your way for a day that is as special as you are.',
+	'May your birthday be filled with sweet moments and wonderful memories.'
+];
+
+const images = [
+	'<img src="https://image.flaticon.com/icons/svg/838/838822.svg" alt="cake">']
